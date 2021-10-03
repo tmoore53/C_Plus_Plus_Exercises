1329342756ea7f9244984506e4d8a27151912a42
tmoore53 <tyler.moore53@outlook.com>
2021-10-02 23:26:30 -0700

    adding CSS 342 example, next step is to add it to remote lab

---
+++ IOAssignment.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+
+    cout << "Hello world. How old are you (in years)?\n";
+
+    cin >> age;
+
+    cout << "That is " << 365 * age << " days.";
+
+    //return 0;
+}
