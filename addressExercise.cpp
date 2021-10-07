34af7a2787736965deaf9cbaa07593815310d918
tmoore53 <tyler.moore53@outlook.com>
2021-10-06 19:30:09 -0700

    Adding a global function after instanciating a class

--- addressExercise.cpp
+++ addressExercise.cpp
@@ -1,13 +1,78 @@
 #include <IOstream>
 
 using namespace std;
+class GlobalFunction
+{
+public:
+    GlobalFunction()
+    {
+        largestNum();
+    }
+
+    void largestNum()
+    {
+        int num1;
+        int num2;
+
+        bool input = true;
+        string answer;
+
+        while (input)
+        {
+            cout << "Please input a number ";
+            cin >> num1; //>> num2;
+            cout << "Please input another number ";
+            cin >> num2;
+
+            if (num1 < num2)
+            {
+                cout << num2 << " is the largest number of the two";
+            }
+            else
+            {
+                cout << num1 << " is the largest number of the two";
+            }
+
+            cout << "Do you want to continue?";
+            cin >> answer;
+            if (answer == "y")
+            {
+                input = true;
+            }
+            else
+            {
+                input = false;
+            }
+        }
+    }
+};
+
+GlobalFunction firstGF;
 
 int main()
 {
 
     int number = 3;
+
+    // * allows you to modify number because it points to the &{variable}
     int *np = &number;
+
+    cout << &number;
+
+    // *np is pointing to the numner's memory address pPrints out memory address.
     cout << *np << endl;
 
-    cout << "Test";
+    cout << "Test" << endl;
+    return 0;
+}
+
+void TestArray()
+{
+    int nums[10];
+    nums[0] = 23;
+    for (int i(0); i < 10; i++)
+    {
+        cout << nums[i] << endl;
+        /* code */
+    }
 }
