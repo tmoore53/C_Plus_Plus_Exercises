40aa8d69cf5e2e877d7c99f37d2252abe4f12251
tmoore53 <tyler.moore53@outlook.com>
2021-12-12 21:21:10 -0800

    Remove even numbers are broken

--- Assignment5/main.cpp
+++ Assignment5/main.cpp
@@ -3,11 +3,14 @@
 using namespace std;
 int main() {
   ThreadedBST test;
-  test.removeEvens(test.getRoot());
+  // This method is limited to 20 nodes
+  // test.removeEvens(test.getRoot());
   cout << test;
-  // ThreadedBST test2;
-  // // test2.removeEvens(test.getRoot());
-  // cout << test2;
+
+  ThreadedBST test2;
+  // This method is limited to 20 nodes
+  // test2.removeEvens(test2.getRoot());
+  cout << test2;
 
   return 0;
 }
--- Assignment5/threadedbst.cpp
+++ Assignment5/threadedbst.cpp
@@ -9,6 +9,7 @@
 
 #include "threadedbst.h"
 #include <iostream>
+#include <queue>
 using namespace std;
 
 int TNode::getData() const { return this->data; }
@@ -18,14 +19,30 @@ TNode::~TNode(){};
 
 int ThreadedBST::getHeight() const { return this->height; }
 
+/**
+ * @brief
+ *  Checks if tree is empty
+ *
+ * @return true
+ * @return false
+ */
 bool ThreadedBST::isEmpty() const {
   if (this->root == nullptr)
     return true;
   return false;
 }
 
+/**
+ * @brief
+ *
+ * @return int : number of node initially creating the tree
+ */
 int ThreadedBST::getNumberOfNodes() const { return this->numberOfNodes; }
 
+/**
+ * @brief Construct a new Threaded B S T:: Threaded B S T object
+ *
+ */
 ThreadedBST::ThreadedBST() {
   cout << "Enter the amount of nodes that the banary tree should have." << endl;
 
@@ -37,6 +54,14 @@ ThreadedBST::ThreadedBST() {
   this->root = addNode(0, getNumberOfNodes() + 1);
 }
 
+/**
+ * @brief
+ * Helper method that returns a balanced tree root
+ *
+ * @param left node
+ * @param right node
+ * @return TNode* : Roor node is returned
+ */
 TNode *ThreadedBST::addNode(int left, int right) {
   if (left + 1 == right)
     return nullptr;
@@ -68,9 +93,17 @@ TNode *ThreadedBST::addNode(int left, int right) {
   return currNode;
 }
 
+/**
+ * @brief
+ * prints the contents of the tree in order
+ *
+ * @param out
+ * @param tBST
+ * @return ostream&
+ */
 ostream &operator<<(ostream &out, const ThreadedBST &tBST) {
-  tBST.threadedPrint(tBST.root);
-  // tBST.inOrderPrint(tBST.root);
+  // tBST.threadedPrint(tBST.root);
+  tBST.inOrderPrint(tBST.root);
   return out << "\n";
 }
 
@@ -79,7 +112,8 @@ void ThreadedBST::inOrderPrint(TNode *node) const {
     inOrderPrint(node->left);
     cout << node->data;
     cout << " ";
-    inOrderPrint(node->right);
+    if (!node->rightThread)
+      inOrderPrint(node->right);
   }
 }
 
@@ -137,13 +171,15 @@ void ThreadedBST::threadedTraverse() {
 
 /**
  * @brief
+ * deletes even numbers from the tree
  *
- * Fix this
+ * ------This function does not fully function------
  *
- * Failing at curr = 17
+ * Current known limitations are that the tree can's have more
+ * than 20 Nodes for this funtion to work
  *
- * setting the parent nodes need to be fixed
- * @param node
+ * setting the parent/right nodes need to be fixed
+ * @param node Takes in the root of the tree
  */
 void ThreadedBST::removeEvens(TNode *node) {
   // Go all the way left
@@ -154,51 +190,89 @@ void ThreadedBST::removeEvens(TNode *node) {
     return;
   }
 
-  TNode *temp;
+  TNode *target;
   TNode *curr = getLeftNode(node);
 
   if (curr->data % 2 == 0) {
-    temp = curr;
+    target = curr;
     curr = curr->right;
-    if (temp->parent != nullptr) {
-      // curr->parent->left = temp->parent;
-      if (temp->parent == curr->right) {
-        if (temp->parent->parent->right == temp->parent)
-          temp->parent->parent->right = curr->parent->parent;
+    if (curr == nullptr) {
+      delete target;
+      return;
+    }
+    if (target->parent != nullptr) {
 
-        if (temp->parent->parent->left == temp->parent)
-          temp->parent->parent->left = curr->parent->parent;
-        curr->parent = temp->parent;
+      // If the node being deleted is also the next value then
+      // set the deleting node children to the current
+      if (target == curr->right) {
+        if (target->parent->parent->right == target->parent)
+          target->parent->parent->right = curr->parent->parent;
+
+        if (target->parent->parent->left == target->parent)
+          target->parent->parent->left = curr->parent->parent;
+        curr->parent = target->parent;
+      } else if (target->left == nullptr && target->rightThread &&
+                 target->right != curr) {
+        curr->right = target->right;
+      } else {
+        curr->parent = curr->parent->parent;
+        if (target == curr->parent->right)
+          curr->parent->right = curr;
+        if (target == curr->parent->left)
+          curr->parent->left = curr;
       }
     }
 
     curr->left = nullptr;
-    delete temp;
-    temp = nullptr;
+    delete target;
+    target = nullptr;
+    this->numberOfNodes--;
   }
 
   while (curr->right != nullptr) {
-    temp = curr->right;
+    target = curr->right;
 
-    if (curr->right->data % 2 == 0 &&
-        (curr->rightThread || curr->left == nullptr)) {
-      if (curr->parent == curr->right) {
-        if (curr->parent->parent->left == curr->right)
-          // This one works in the begining and not at 17
-          curr->parent->parent->left = curr;
-        if (curr->parent->parent->right == curr->right)
-          curr->parent->parent->right = curr;
+    if (target->data % 2 == 0 && (curr->rightThread || curr->left == nullptr)) {
 
-        // curr->right->right->parent = curr;
+      if (target->parent != nullptr) {
+        if (target == curr->parent) {
+          curr->parent = target->parent;
+          if (curr->data > curr->parent->data) {
+            curr->parent->right = curr;
+          } else if (curr->data < curr->parent->data) {
+            curr->parent->left = curr;
+          }
+          curr->right = target->right;
+          curr->right->parent = curr;
+          if (curr != target->left && target->right != nullptr)
+            curr->left = target->left;
+        } else {
+          curr->parent = target->parent;
+          if (target->data < target->parent->data) {
+            target->parent->left = target->left;
+            curr->right = target->right;
+          } else if (target->data > target->parent->data) {
+            target->parent->right = target->right;
+          }
+
+          if (target->left != nullptr) {
+            target->left->parent = curr;
+            curr->right = target->left;
+          }
+          if (curr->right != nullptr) {
+            curr->right = target->right;
+          }
+          if (!target->rightThread && target->right != nullptr)
+            target->right->parent = curr;
+        }
       }
-      bool transfer = temp->rightThread && temp != root;
+      bool transfer = target->rightThread && target != root;
       if (curr->right != nullptr)
         curr->rightThread = transfer;
 
-      if (curr->left != nullptr)
+      if (curr->left != nullptr && curr->left != nullptr)
         curr->left = curr->left->left;
-      curr->right = curr->right->right;
-      if (temp == root) {
+      if (target == root) {
         curr->right = getLeftNode(root->right);
         root->right->parent = root->left;
         root = root->left;
@@ -206,13 +280,14 @@ void ThreadedBST::removeEvens(TNode *node) {
       }
       curr = curr->right;
 
-      delete temp;
-      temp = nullptr;
+      delete target;
+      target = nullptr;
+      this->numberOfNodes--;
     }
 
     else if (curr->right->data % 2 != 0 && curr->right != root) {
-      curr = getLeftNode(curr->right);
-      removeEvens(curr);
+      // curr = getLeftNode(curr->right);
+      removeEvens(curr->right);
       break;
     } else {
       curr = getLeftNode(curr->right);
@@ -238,3 +313,113 @@ void ThreadedBST::destructorHelper(TNode *&node) {
   delete node;
   node = nullptr;
 }
+
+// /* function to delete element in binary tree */
+// TNode *ThreadedBST::deletion(TNode *root, int val) {
+//   if (root == nullptr)
+//     return nullptr;
+
+//   if (root->left == nullptr && root->right == nullptr) {
+//     if (root->data == val)
+//       return nullptr;
+//     else
+//       return root;
+//   }
+
+//   queue<TNode *> q;
+//   q.push(root);
+
+//   TNode *temp;
+//   TNode *key_node = nullptr;
+
+//   // Do level order traversal to find deepest
+//   // node(temp) and node to be deleted (key_node)
+//   while (!q.empty()) {
+//     temp = q.front();
+//     q.pop();
+
+//     if (temp->data == val) {
+//       key_node = temp;
+//       break;
+//     }
+
+//     if (temp->left != nullptr)
+//       q.push(temp->left);
+
+//     if (temp->right != nullptr && !temp->rightThread)
+//       q.push(temp->right);
+//   }
+
+//   if (key_node != nullptr) {
+//     int x = temp->data;
+//     deletDeepest(root, temp);
+//     key_node->data = x;
+//   }
+//   return root;
+// }
+
+// void ThreadedBST::copyConstructor(TNode *root, TNode *d_node) {
+//   // Temporary queue
+//   queue<TNode *> q;
+//   // Push the root
+//   q.push(root);
+
+//   // Do level order traversal until last node
+//   TNode *temp;
+//   while (!q.empty()) {
+//     temp = q.front();
+//     q.pop();
+//     if (d_node->data % 2 == 0) {
+//       temp = nullptr;
+//       delete d_node;
+//       return;
+//     }
+//     if (temp->right != nullptr && !temp->rightThread) {
+//       if (d_node->data % 2 == 0) {
+//         temp->right = nullptr;
+//         delete d_node;
+//         return;
+//       } else
+//         q.push(temp->right);
+//     }
+
+//     if (temp->left != nullptr) {
+//       if (d_node->data % 2 == 0) {
+//         temp->left = nullptr;
+//         delete d_node;
+//         return;
+//       } else
+//         q.push(temp->left);
+//     }
+//   }
+// }
+
+// void ThreadedBST::copyConstructor(TNode *root, TNode *d_node) {
+//   vector<int> retVec;
+//   // Temporary queue
+//   queue<TNode *> q;
+//   // Push the root
+//   q.push(root);
+
+//   // Do level order traversal until last node
+//   TNode *temp;
+//   while (!q.empty()) {
+//     temp = q.front();
+//     q.pop();
+
+//     if (temp->left != nullptr) {
+//       copyConstructor(temp->left, root);
+//       // q.push(temp->left);
+//     }
+
+//     if (temp->right != nullptr && !temp->rightThread) {
+//       copyConstructor(temp->right, root);
+
+//       // q.push(temp->right);
+//     }
+//     retVec.push_back(temp->data);
+//   }
+//   // return retVec;
+// }
+
+// ThreadedBST &ThreadedBST::operator=(const ThreadedBST &original) {}
