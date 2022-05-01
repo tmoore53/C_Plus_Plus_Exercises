c1131c9a458a27f7db20d0a5111d3a02e041c875
tmoore53 <tyler.moore53@outlook.com>
2022-04-30 20:38:01 -0700

    C++ code to mimic ARM functionality

--- Final343/main.cpp
+++ Final343/main.cpp
@@ -69,11 +69,8 @@ void testCat()
     cout << *a << endl;
 }
 
-int main()
+void bitMinip_()
 {
-    cout << "Ready For the FInal?" << endl;
-    // vTest();
-    // testCat();
     vector<char> ch = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'};
 
     cout << "Before any Changes" << endl;
@@ -139,6 +136,121 @@ int main()
     num--;
 
     cout << "Num = " << num << endl;
+}
+
+void multiplyByShifting()
+{
+    int R0 = 30;
+    cout << "With multiply by 10: " << R0 * 10 << endl;
+    R0 = 30;
+    int R1 = R0 << 3;
+    int R2 = R0 << 1;
+    R0 = R1 + R2;
+    cout << "With bit shift: " << R0 << endl;
+
+    // 100 (Base 10) = 0110 0100 (Base 2)
+    //  0       1       1       0       0       1       0       0
+    // 0*2^7 + 1*2^6 + 1*2^5 + 0*2^4 + 0*2^3 + 1*2^2 + 0*2^1 + 0*2^0
+    //    0   +   64  +   32  +   0   +  0   +  4    +   0  +   0  =
+    R0 = 30;
+    cout << "With multiply by 100: " << R0 * 100 << endl;
+    R1 = 0;
+    R2 = 0;
+    R0 = 30;
+    R1 = R0 << 6;
+    int R3 = R0 << 5;
+    int R4 = R0 << 2;
+    // R2 = R0 << 2;
+    R0 = R1 + R3 + R4;
+    cout << "With bit shift: " << R0 << endl;
+
+    cout << "With bit shift in one line: " << R0 << endl;
+}
+
+void logicalOps()
+{
+    int R0 = 20;
+    int R1 = 10;
+    int R2 = 5;
+    int R3, R4 = 0;
+
+    if (R0 >= R1)
+    {
+        R3 = R0 - R2;
+    }
+    // R0 >= R2
+    // R3 >= 0
+    if (R3 >= 0)
+    {
+        R0 = 25;
+    }
+    if (R3 < 0)
+    {
+        R4 = R2 - R0;
+    }
+    if (R4 < 0)
+    {
+        R0 = 15;
+    }
+
+    cout << "R0: " << R0 << " R1: " << R1 << " R2: " << R2 << " R3: " << R3 << " R4: " << R4 << endl;
+
+    int R0 = 20;
+    int R1 = 10;
+    int R2 = 5;
+    int R3 = 0;
+    int R4 = 0;
+
+    if (R0 >= R1)
+    {
+        R3 = R0 - R2;
+        if (R3 >= 0)
+        {
+            R0 = 25;
+        }
+    }
+    else
+    {
+        R4 = R2 - R0;
+        if (R4 < 0)
+        {
+            R0 = 15;
+        }
+    }
+
+    cout << "R0: " << R0 << " R1: " << R1 << " R2: " << R2 << " R3: " << R3 << " R4: " << R4 << endl;
+
+    R0 = 10;
+    R1 = 20;
+    R2 = 5;
+    R3 = 0;
+    R4 = 0;
+
+    if (R0 >= R1)
+    {
+        R3 = R0 - R2;
+        if (R3 >= 0)
+        {
+            R0 = 25;
+        }
+    }
+    else if (R0 < R1)
+    {
+        R4 = R2 - R0;
+        if (R4 < 0)
+        {
+            R0 = 15;
+        }
+    }
+
+    cout << "R0: " << R0 << " R1: " << R1 << " R2: " << R2 << " R3: " << R3 << " R4: " << R4 << endl;
+}
+
+int main()
+{
+    cout << "Ready For the FInal?" << endl;
+    // vTest();
+    // testCat();
 
     return -1;
 }
