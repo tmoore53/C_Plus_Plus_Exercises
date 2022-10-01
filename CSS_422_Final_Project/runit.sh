555470f795d24323588eb9908836c52b7bd76d68
tmoore53 <tyler.moore53@outlook.com>
2022-10-01 16:23:44 -0700

    Cleanup of repo

--- CSS_422_Final_Project/runit.sh
+++ CSS_422_Final_Project/runit.sh
@@ -2,8 +2,8 @@
 
 gcc driver_cpg.c heap.c -o ./a.out
 
-gcc driver_cpg.c heap-2.c -o ./b.out
+#gcc driver_cpg.c heap-2.c -o ./b.out
 
 ./a.out > output.txt
-
-./b.out > output2.txt
+./a.out
+#./b.out > output2.txt
