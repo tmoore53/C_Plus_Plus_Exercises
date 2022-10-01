02116ee2ad12c3d6a858d7b982126cb3fed998b9
tmoore53 <tyler.moore53@outlook.com>
2022-10-01 16:26:52 -0700

    claening repo again

--- CSS_422_Final_Project/runit.sh
+++ CSS_422_Final_Project/runit.sh
@@ -2,15 +2,8 @@
 
 gcc driver_cpg.c heap.c -o ./a.out
 
-<<<<<<< HEAD
 #gcc driver_cpg.c heap-2.c -o ./b.out
 
 ./a.out > output.txt
 ./a.out
 #./b.out > output2.txt
-=======
-
-
-./a.out > output.txt
-
->>>>>>> 1e75d296b041f6663016c5badc67f4f89787ef36
